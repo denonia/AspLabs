1cb8a4e2abb35086940ff2309d385a9f497ca326
denonia <denoniaa@gmail.com>
2024-02-26 22:17:43 +0200

    Lab4 finished

---
+++ Lab4/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Lab4.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly ValidUntil { get; set; }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -1,3 +1,5 @@
+using Lab4.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+builder.Configuration.AddJsonFile("Properties/books.json");
+builder.Configuration.AddJsonFile("Properties/users.json");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -21,5 +26,21 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Map("/Library", () => "Welcome to the library :D Visit /Books or /Profile");
+
+app.Map("/Library/Books", (IConfiguration config) =>
+{
+    var books = new List<Book>();
+    config.GetSection("Books").Bind(books);
+    return books;
+});
+
+app.Map("/Library/Profile/{id:int:range(0,5)?}",  (IConfiguration config, int? id) =>
+{
+    var users = new List<User>();
+    config.GetSection("Users").Bind(users);
+    return users.First(u => u.Id == (id ?? 0));
+});
 
 app.Run();
