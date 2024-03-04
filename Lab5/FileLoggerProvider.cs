e58c77bf20d5435e235b5a424bfe6927a8e7ff6b
denonia <denoniaa@gmail.com>
2024-03-04 22:40:08 +0200

    Lab5 finished

---
+++ Lab5/FileLoggerProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace Lab5;
+
+public class FileLoggerProvider : ILoggerProvider
+{
+    string path;
+
+    public FileLoggerProvider(string path)
+    {
+        this.path = path;
+    }
+
+    public ILogger CreateLogger(string categoryName)
+    {
+        return new FileLogger(path);
+    }
+
+    public void Dispose()
+    {
+    }
+}
--- Lab5/Program.cs
+++ Lab5/Program.cs
@@ -1,3 +1,5 @@
+using Lab5;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Logging.AddFile(Path.Combine(Directory.GetCurrentDirectory(), "errors.txt"));
 
 var app = builder.Build();
 
@@ -16,10 +20,45 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => Results.Extensions.Html(
+    @"<form method='POST'>
+        <input type='text' name='value'>
+        <input type='datetime-local' name='datetime'>
+        <input type='submit'>
+      </form>"
+));
+
+app.MapPost("/", async (HttpContext ctx) =>
+{
+    var value = ctx.Request.Form["value"];
+    var dateTime = DateTime.Parse(ctx.Request.Form["datetime"]);
+
+    ctx.Response.Cookies.Append("value", value, new CookieOptions { Expires = dateTime });
+    
+    ctx.Response.Redirect("/check");
+});
+
+app.MapGet("/check", async (ctx) =>
+{
+    if (ctx.Request.Cookies.ContainsKey("value"))
+    {
+        var cookie = ctx.Request.Cookies["value"];
+        await ctx.Response.WriteAsync($"value set: '{cookie}'");
+    }
+    else
+    {
+        await ctx.Response.WriteAsync("no cookie set");
+    }
+});
+
+app.MapGet("/unset", async (ctx) =>
+{
+    ctx.Response.Cookies.Delete("value");
+    ctx.Response.Redirect("/");
+});
 
 app.Run();
